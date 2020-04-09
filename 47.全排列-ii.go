3ac5a7e081876fdffa405623a7e97a4b3dd23762
limi0713 <1273700520@qq.com>
2020-04-09 15:13:06 +0800

    回溯剪枝

---
+++ 47.全排列-ii.go
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=47 lang=golang
+ *
+ * [47] 全排列 II
+ *
+ * https://leetcode-cn.com/problems/permutations-ii/description/
+ *
+ * algorithms
+ * Medium (57.84%)
+ * Likes:    258
+ * Dislikes: 0
+ * Total Accepted:    49.7K
+ * Total Submissions: 85.8K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,1,2]
+ * 输出:
+ * [
+ * ⁠ [1,1,2],
+ * ⁠ [1,2,1],
+ * ⁠ [2,1,1]
+ * ]
+ * 
+ */
+
+// @lc code=start
+func dfs(ans *[][]int, nums, cur []int, hadAdd map[int]bool){
+	if len(cur) == len(nums) {
+		*ans = append(*ans, cur)
+		return
+	}
+
+	for i := range nums {
+		if  _, ok := hadAdd[i-1]; i > 0 && nums[i] == nums[i-1] && !ok{
+			continue
+		}
+
+		if _, ok := hadAdd[i]; ok {
+			continue
+		}
+
+		temp := make([]int, 0)
+		temp = append(temp, cur...)
+		temp = append(temp, nums[i])
+
+		hadAdd[i] = true
+
+		dfs(ans, nums, temp, hadAdd)
+		
+		delete(hadAdd, i)
+	}
+
+}
+
+func permuteUnique(nums []int) [][]int {
+
+	sort.Ints(nums)
+
+	ans := make([][]int, 0)
+
+	dfs(&ans, nums, []int{}, make(map[int]bool))
+
+	return ans
+}
+// @lc code=end
+
