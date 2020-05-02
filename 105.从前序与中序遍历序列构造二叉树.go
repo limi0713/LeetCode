3d571c83746908d242f33b4834e2044579e34e92
limi0713 <1273700520@qq.com>
2020-05-03 01:25:19 +0800

    二叉树恢复

---
+++ 105.从前序与中序遍历序列构造二叉树.go
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=105 lang=golang
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ *
+ * https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (65.08%)
+ * Likes:    425
+ * Dislikes: 0
+ * Total Accepted:    62.1K
+ * Total Submissions: 95.4K
+ * Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+ *
+ * 根据一棵树的前序遍历与中序遍历构造二叉树。
+ * 
+ * 注意:
+ * 你可以假设树中没有重复的元素。
+ * 
+ * 例如，给出
+ * 
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 
+ * 返回如下的二叉树：
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+
+	node := &TreeNode{
+		Val:preorder[0],
+	}
+
+	leftLen := 0
+	for i := range inorder {
+		if inorder[i] == preorder[0] {
+			leftLen = i
+			break
+		}
+	}
+
+	node.Left = buildTree(preorder[1:leftLen+1],inorder[:leftLen])
+	node.Right = buildTree(preorder[leftLen+1:],inorder[leftLen+1:])
+	return node
+}
+// @lc code=end
+
