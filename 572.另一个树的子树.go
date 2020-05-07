7507abd156e733719358388b835b1b965c827c59
limi0713 <1273700520@qq.com>
2020-05-08 00:08:41 +0800

    DFS-572.另一个树的子树

---
+++ 572.另一个树的子树.go
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=golang
+ *
+ * [572] 另一个树的子树
+ *
+ * https://leetcode-cn.com/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (44.60%)
+ * Likes:    265
+ * Dislikes: 0
+ * Total Accepted:    36.9K
+ * Total Submissions: 78.8K
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s
+ * 也可以看做它自身的一棵子树。
+ * 
+ * 示例 1:
+ * 给定的树 s:
+ * 
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * 
+ * 给定的树 t：
+ * 
+ * 
+ * ⁠  4 
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * 
+ * 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+ * 
+ * 示例 2:
+ * 给定的树 s：
+ * 
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * ⁠   /
+ * ⁠  0
+ * 
+ * 
+ * 给定的树 t：
+ * 
+ * 
+ * ⁠  4
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * 
+ * 返回 false。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSubtree(s *TreeNode, t *TreeNode) bool {
+	if s == nil {
+		return t == nil
+	}
+	return check(s,t) || isSubtree(s.Left,t) || isSubtree(s.Right,t)
+}
+
+func check(s, t *TreeNode) bool {
+	if s == nil {
+		return t == nil
+	}
+
+	if t == nil {
+		return s == nil
+	}
+
+	if s.Val != t.Val {
+		return false
+	}
+
+	return check(s.Left,t.Left) && check(s.Right,t.Right)
+}
+
+// @lc code=end
+
